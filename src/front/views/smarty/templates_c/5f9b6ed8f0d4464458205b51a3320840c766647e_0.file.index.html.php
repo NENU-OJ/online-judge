d8a03c9e3077b6fa4f03ffb66f79dacbfdcf52c5
shihao <shihao_231347@hotmail.com>
2017-08-15 15:18:31 +0800

    整合smarty框架

---
+++ src/front/views/smarty/templates_c/5f9b6ed8f0d4464458205b51a3320840c766647e_0.file.index.html.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.31, created on 2017-08-15 07:15:21
+  from "/var/www/html/OnlineJudge/src/front/views/smarty/templates/index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_59929f89c7e684_00200890',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5f9b6ed8f0d4464458205b51a3320840c766647e' => 
+    array (
+      0 => '/var/www/html/OnlineJudge/src/front/views/smarty/templates/index.html',
+      1 => 1502781297,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_59929f89c7e684_00200890 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>website</title>
+</head>
+<body>
+<h1><?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+</h1>
+<h2><?php echo $_smarty_tpl->tpl_vars['str']->value;?>
+</h2>
+</body>
+</html><?php }
+}
