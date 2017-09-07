ffb80644fa905d07e482af8d794e23a057d929d9
ToRapture <ToRapture@gmail.com>
2017-09-07 16:30:17 +0800

    python后端

--- src/backend/judger/SocketHandler.py
+++ src/backend/judger/SocketHandler.py
@@ -1,5 +1,23 @@
-
+import socket
 
 class SocketHandler(object):
-    def __init__(self):
-        pass
+    def __init__(self, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(('127.0.0.1', port))
+        self.server.listen()
+
+    def get_rid(self):
+        sock, addr = self.server.accept()
+        rid = sock.recv(1024).decode('utf-8')
+        sock.close()
+        return rid
+
+
+def main():
+    socket_handler = SocketHandler(27016)
+    for i in range(5):
+        rid = socket_handler.get_rid()
+        print(rid)
+
+if __name__ == '__main__':
+    main()
